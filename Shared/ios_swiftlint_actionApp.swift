d5c447809a92299b3e71f6b1a3b03cc3b55544f3
Zaim Ramlan <zaem.muz@gmail.com>
2020-08-03 10:27:39 +0800

    add sample app

---
+++ Shared/ios_swiftlint_actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swiftlint_actionApp.swift
+//  Shared
+//
+//  Created by Zaim Ramlan on 24/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct ios_swiftlint_actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
